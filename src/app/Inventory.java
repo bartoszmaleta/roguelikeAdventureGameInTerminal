ebb2ca592c97740754b14c7b592a4ad20750db5d
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-12 14:25:07 +0100

    Add basic inventory constructor

--- src/app/Inventory.java
+++ src/app/Inventory.java
@@ -1,6 +1,14 @@
 package app;
 
+import java.util.ArrayList;
+
+import app.items.Item;
+
 public class Inventory {
-    
+    private ArrayList<Item> inventory;
+
+    public Inventory() {
+        inventory = new ArrayList<>();
+    }
 
 }
