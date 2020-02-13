2af1161c7fc9fa7882f93f357863b430c650b0d5
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-13 23:58:03 +0100

    Add constructor of Dragon

--- src/app/creatures/Dragon.java
+++ src/app/creatures/Dragon.java
@@ -6,10 +6,11 @@ import app.Coordinates;
 import app.inventory.Inventory;
 
 public class Dragon extends Monster {
+    private Inventory dragonInventory;
 
-    public Dragon(String apparel, List<Coordinates> coordinatesList, String name, int health, Inventory inventory,
+    public Dragon(List<Coordinates> coordinatesList, String name, int health, Inventory inventory,
             int damage, int experience) {
-        super(apparel, coordinatesList, name, health, inventory, damage, experience);
+        super("*", coordinatesList, "Dragon", 30, inventory, 10, 150);
     }
 
 }
