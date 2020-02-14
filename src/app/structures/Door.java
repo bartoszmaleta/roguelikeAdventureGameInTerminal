222beae57d0aced419ae35863080ac118cafd799
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-14 12:22:34 +0100

    Delete redundant code

--- src/app/structures/Door.java
+++ src/app/structures/Door.java
@@ -11,7 +11,7 @@ public class Door extends Sprite {
         public Door( List<Coordinates> coordinatesList, String keyIdNeeded) {
             super("\u26E9", coordinatesList);
             this.keyIdNeeded = keyIdNeeded;
-            // keyIdNeeded = "asd123"
+            // keyIdNeeded = "asd123"  // HERE OR IN CONSTRUCTOR!!
         }
     
         public String getKeyIdNeeded() {
